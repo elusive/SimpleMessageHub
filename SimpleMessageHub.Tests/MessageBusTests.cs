a5a816ce334ec5d0565dd652b12118330840c074
John Gilliland <John.Gilliland@Starz.com>
2023-05-11 14:20:42 -0600

    Add another test for message hub.

--- SimpleMessageHub.Tests/MessageBusTests.cs
+++ SimpleMessageHub.Tests/MessageBusTests.cs
@@ -1,5 +1,6 @@
 namespace SimpleMessageHub.Tests
 {
+    using SimpleMessageHub.Messages;
     using SimpleMessageHub.Tests.Shared;
 
     using System;
@@ -36,6 +37,26 @@ namespace SimpleMessageHub.Tests
 
             // assert
             Assert.Equal(2, handledCount);
+        }
+
+        [Fact]
+        public void TestPublishOnlyCallsHandlerForExpectedMessageType()
+        {
+            // arrange
+            const int expectedCount = 1;
+            var actualCount = 0;
+            var msg1 = new TestMessage("Title1");
+            var msg2 = new TestMessage("TestMessage2");
+            var msg3 = new StringMessage("A string message");
+            _messageHub.Subscribe((StringMessage m) => actualCount++);
+
+            // act
+            _messageHub.Publish(msg1);
+            _messageHub.Publish(msg2);
+            _messageHub.Publish(msg3);
+
+            // assert
+            Assert.Equal(expectedCount, actualCount);
         }
     }
 }
